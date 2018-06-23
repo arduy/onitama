48338c4cdf203a1ea5ddb9852c6cdd1263571adc
Adrian Duynstee <adrian.duy@gmail.com>
2018-06-22 17:15:32 -0700

    Test initial piece locations

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 import cards
-from onitama import Game, Move, Player
+from onitama import Game, Move, Player, Piece
 
 
 class TestMoves(unittest.TestCase):
@@ -11,6 +11,14 @@ class TestMoves(unittest.TestCase):
         self.assertEqual(Player.BLUE, self.game.active_player)
         self.assertEqual(set(self.game.cards[Player.RED]), {cards.MONKEY, cards.ELEPHANT})
         self.assertEqual(set(self.game.cards[Player.BLUE]), {cards.CRANE, cards.MANTIS})
+        self.assertEqual(self.game.neutral_card, cards.TIGER)
+        red_pawns = [(x, 0) for x in [0, 1, 3, 4]]
+        blue_pawns = [(x, 4) for x in [0, 1, 3, 4]]
+        for (loc1, loc2) in zip(red_pawns, blue_pawns):
+            self.assertEqual(self.game.board.get(loc1), Piece.R_PAWN)
+            self.assertEqual(self.game.board.get(loc2), Piece.B_PAWN)
+        self.assertEqual(self.game.board.get((2, 0)), Piece.R_KING)
+        self.assertEqual(self.game.board.get((2, 4)), Piece.B_KING)
 
 
 if __name__ == '__main__':
