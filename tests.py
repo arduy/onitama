c08b06e75c65f97d7ab1f2d86b592411a694aad6
Adrian Duynstee <adrian.duy@gmail.com>
2018-07-03 16:30:27 -0700

    Refactor: move contents of cards.py to onitama.py (combine modules)

--- tests.py
+++ tests.py
@@ -1,18 +1,17 @@
 import unittest
-import cards
 from onitama import Game, Move, Player, Piece, BoardBoundsError
 import onitama
 
 
 class TestGame(unittest.TestCase):
     def setUp(self):
-        self.game = Game([cards.MONKEY, cards.ELEPHANT, cards.CRANE, cards.MANTIS, cards.TIGER])
+        self.game = Game([onitama.MONKEY, onitama.ELEPHANT, onitama.CRANE, onitama.MANTIS, onitama.TIGER])
 
     def test_init(self):
         self.assertEqual(Player.BLUE, self.game.active_player)
-        self.assertEqual(set(self.game.cards[Player.RED]), {cards.MONKEY, cards.ELEPHANT})
-        self.assertEqual(set(self.game.cards[Player.BLUE]), {cards.CRANE, cards.MANTIS})
-        self.assertEqual(self.game.neutral_card, cards.TIGER)
+        self.assertEqual(set(self.game.cards[Player.RED]), {onitama.MONKEY, onitama.ELEPHANT})
+        self.assertEqual(set(self.game.cards[Player.BLUE]), {onitama.CRANE, onitama.MANTIS})
+        self.assertEqual(self.game.neutral_card, onitama.TIGER)
         red_pawns = [(x, 0) for x in [0, 1, 3, 4]]
         blue_pawns = [(x, 4) for x in [0, 1, 3, 4]]
         for loc1, loc2 in zip(red_pawns, blue_pawns):
@@ -22,17 +21,17 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.board.get((2, 4)), Piece.B_KING)
 
     def test_moves(self):
-        moves = [Move(Player.RED, (1, 1), (1, 4), card) for card in cards.ALL_CARDS]
+        moves = [Move(Player.RED, (1, 1), (1, 4), card) for card in onitama.ALL_CARDS]
         for move in moves:
             self.assertFalse(move.validate())
-        moves = [Move(Player.RED, (2, 2), (2, 4), card) for card in cards.ALL_CARDS]
+        moves = [Move(Player.RED, (2, 2), (2, 4), card) for card in onitama.ALL_CARDS]
         legal_moves = set(filter(lambda x: x.validate(), moves))
         # TIGER is only card with the move vector (0, 2)
-        self.assertEqual(legal_moves, set(filter(lambda x: x.card == cards.TIGER, moves)))
-        moves = [Move(Player.RED, (3, 1), (2, 2), card) for card in cards.ALL_CARDS]
+        self.assertEqual(legal_moves, set(filter(lambda x: x.card == onitama.TIGER, moves)))
+        moves = [Move(Player.RED, (3, 1), (2, 2), card) for card in onitama.ALL_CARDS]
         legal_moves = set(filter(lambda x: x.validate(), moves))
         # The only cards with move vector (-1, 1)
-        legal_cards = [cards.MONKEY, cards.MANTIS, cards.ELEPHANT, cards.EEL, cards.GOOSE, cards.FROG]
+        legal_cards = [onitama.MONKEY, onitama.MANTIS, onitama.ELEPHANT, onitama.EEL, onitama.GOOSE, onitama.FROG]
         self.assertEqual(legal_moves, {move for move in moves if move.card in legal_cards})
 
     def test_board(self):
@@ -41,33 +40,33 @@ class TestGame(unittest.TestCase):
             self.assertRaises(BoardBoundsError, self.game.board.get, loc)
             self.assertRaises(BoardBoundsError, self.game.board.set, loc, Piece.EMPTY)
         # Attempt to move Red's pieces with blue
-        bad_moves = [Move(Player.BLUE, (x, 0), (x, 2), cards.TIGER) for x in range(5)]
-        good_moves = [Move(Player.RED, (x, 0), (x, 2), cards.TIGER) for x in range(5)]
+        bad_moves = [Move(Player.BLUE, (x, 0), (x, 2), onitama.TIGER) for x in range(5)]
+        good_moves = [Move(Player.RED, (x, 0), (x, 2), onitama.TIGER) for x in range(5)]
         for bad, good in zip(bad_moves, good_moves):
             self.assertFalse(self.game.board.validate_move(bad))
             self.assertTrue(self.game.board.validate_move(good))
-        move = Move(Player.RED, (0, 0), (-1, 0), cards.ROOSTER)
+        move = Move(Player.RED, (0, 0), (-1, 0), onitama.ROOSTER)
         self.assertFalse(self.game.board.validate_move(move))
-        move = Move(Player.RED, (1, 0), (0, 0), cards.ROOSTER)
+        move = Move(Player.RED, (1, 0), (0, 0), onitama.ROOSTER)
         self.assertFalse(self.game.board.validate_move(move))
         self.game.board.set((0, 0), Piece.EMPTY)
         self.assertTrue(self.game.board.validate_move(move))
 
     def test_do_move(self):
-        move = Move(Player.BLUE, (2, 4), (2, 3), cards.CRANE)
+        move = Move(Player.BLUE, (2, 4), (2, 3), onitama.CRANE)
         self.game.do_move(move)
         self.assertEqual(self.game.board.get((2, 3)), Piece.B_KING)
         self.assertEqual(self.game.board.get((2, 4)), Piece.EMPTY)
         self.assertEqual(self.game.active_player, Player.RED)
-        self.assertEqual(self.game.neutral_card, cards.CRANE)
+        self.assertEqual(self.game.neutral_card, onitama.CRANE)
         self.assertFalse((2, 4) in self.game.kings[Player.BLUE])
         self.assertTrue((2, 3) in self.game.kings[Player.BLUE])
-        move = Move(Player.RED, (0, 0), (1, 1), cards.MONKEY)
+        move = Move(Player.RED, (0, 0), (1, 1), onitama.MONKEY)
         self.game.do_move(move)
         self.assertEqual(self.game.board.get((0, 0)), Piece.EMPTY)
         self.assertEqual(self.game.board.get((1, 1)), Piece.R_PAWN)
         self.assertEqual(self.game.active_player, Player.BLUE)
-        self.assertEqual(self.game.neutral_card, cards.MONKEY)
+        self.assertEqual(self.game.neutral_card, onitama.MONKEY)
         self.assertTrue((0, 0) not in self.game.pawns[Player.RED] and (1, 1) in self.game.pawns[Player.RED])
 
     def test_do_parsed_moves(self):
@@ -77,14 +76,14 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.board.get((2, 3)), Piece.B_KING)
         self.assertEqual(self.game.board.get((2, 4)), Piece.EMPTY)
         self.assertEqual(self.game.active_player, Player.RED)
-        self.assertEqual(self.game.neutral_card, cards.CRANE)
+        self.assertEqual(self.game.neutral_card, onitama.CRANE)
         self.assertFalse((2, 4) in self.game.kings[Player.BLUE])
         self.assertTrue((2, 3) in self.game.kings[Player.BLUE])
         self.game.do_move(moves[1])
         self.assertEqual(self.game.board.get((0, 0)), Piece.EMPTY)
         self.assertEqual(self.game.board.get((1, 1)), Piece.R_PAWN)
         self.assertEqual(self.game.active_player, Player.BLUE)
-        self.assertEqual(self.game.neutral_card, cards.MONKEY)
+        self.assertEqual(self.game.neutral_card, onitama.MONKEY)
         self.assertTrue((0, 0) not in self.game.pawns[Player.RED] and (1, 1) in self.game.pawns[Player.RED])
 
     def test_bad_move_string(self):
