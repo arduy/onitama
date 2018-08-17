4a09fd978e077e30617740530479a5888e6ebc22
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-16 18:46:04 -0700

    Basics of AI module: efficient representations of game state

---
+++ ai.py
@@ -0,0 +1,69 @@
+import onitama as oni
+from collections import namedtuple
+
+RED = 0
+BLUE = 1
+
+EMPTY = 0
+REDPAWN = 1
+REDKING = 2
+BLUEPAWN = -1
+BLUEKING = -2
+
+class AI:
+    def __init__(self, game):
+        self.game = game
+        self.root = node_from_game(game)
+
+    def generate_search_space(self, depth):
+        pass
+
+    def get_nodes(self, depth):
+        if depth == 0:
+            return [self.root]
+        else:
+            return []
+
+'''
+A board is a length 25 array of ints, each index i representing
+the coordinate (x, y) = (i % 5, i // 5)
+
+The values in the array represent what is contained at each square
+See above for integer meanings (EMPTY = 0, REDPAWN = 1, etc)
+
+Moves have start and end squares as integers (same convention) with
+the player performing the move identified by RED = 0, BLUE = 1
+'''
+
+Node = namedtuple('Node', ['board', 'last_move'])
+Move = namedtuple('Move', ['start', 'end', 'player', 'card'])
+Card = namedtuple('Card', ['moves', 'start_player'])
+
+def card_from_onicard(card):
+    red_moves = set(c[0]+c[1]*5 for c in card.moves[oni.Player.RED])
+    blue_moves = set(c[0]+c[1]*5 for c in card.moves[oni.Player.BLUE])
+    return Card(
+        moves=(red_moves, blue_moves),
+        start_player=RED if card.start_player == oni.Player.RED else BLUE,
+    )
+
+def move_from_onimove(move):
+    return Move(
+        start=move.start[0] + move.start[1]*5,
+        end=move.end[0] + move.end[1]*5,
+        player=RED if move.player == oni.Player.RED else BLUE,
+        card=card_from_onicard(move.card)
+    )
+
+def node_from_game(game):
+    pieces = {
+        oni.Piece.EMPTY: EMPTY,
+        oni.Piece.R_PAWN: REDPAWN,
+        oni.Piece.R_KING: REDKING,
+        oni.Piece.B_PAWN: BLUEPAWN,
+        oni.Piece.B_KING: BLUEKING,
+    }
+    return Node(
+        board=[pieces[p] for p in game.board.array],
+        last_move=None if len(game.moves) == 0 else move_from_onimove(game.moves[-1]),
+    )
---
+++ aitests.py
@@ -0,0 +1,41 @@
+import unittest
+import onitama as oni
+import ai
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = oni.Game([oni.TIGER, oni.TIGER, oni.TIGER, oni.TIGER, oni.TIGER])
+        self.ai = ai.AI(game=self.game)
+
+    def test_node_from_game(self):
+        node = ai.node_from_game(game=self.game)
+        self.assertEqual(node.last_move, None)
+        self.assertEqual(len(node.board), 25)
+        for i, p in enumerate(node.board):
+            mapping = {
+                ai.EMPTY: oni.Piece.EMPTY,
+                ai.REDKING: oni.Piece.R_KING,
+                ai.REDPAWN: oni.Piece.R_PAWN,
+                ai.BLUEKING: oni.Piece.B_KING,
+                ai.BLUEPAWN: oni.Piece.B_PAWN,
+            }
+            self.assertEqual(mapping[p], self.game.board.array[i])
+        self.game.do_move(oni.Move(
+            player=oni.Player.BLUE,
+            start=(0,4),
+            end=(0,2),
+            card=oni.TIGER,
+        ))
+        node = ai.node_from_game(game=self.game)
+        self.assertEqual(node.last_move.start, 20)
+        self.assertEqual(node.last_move.end, 10)
+        self.assertEqual(node.last_move.player, ai.BLUE)
+
+    def test_search(self):
+        self.ai.generate_search_space(depth=3)
+        self.assertEqual(len(self.ai.get_nodes(depth=0)), 1)
+        # self.assertEqual(len(self.ai.get_nodes(depth=1)), 5)
+        # self.assertEqual(len(self.ai.get_nodes(depth=2)), 25)
+
+if __name__ == '__main__':
+    unittest.main()
