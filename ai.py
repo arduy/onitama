e4297d848f4a25c5da4b2f54e217910a795889a4
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-21 15:30:06 -0700

    Refactoring for cleanliness

--- ai.py
+++ ai.py
@@ -15,65 +15,64 @@ REDGOAL = 22
 BLUEGOAL = 2
 
 class AI:
-    def __init__(self, game):
-        self.game = game
-        self.root = node_from_game(game)
-        self.cards = tuple(card_from_onicard(card) for card in game.start_cards)
+    def __init__(self):
+        pass
 
-    def generate_search_space(self, depth):
-        def apply_move(node, move):
-            new_board = node.board[:]
-            if move.player == BLUE:
-                if (new_board[move.end] == REDKING
-                    or (new_board[move.start] == BLUEKING and move.end == BLUEGOAL)):
-                    gameover = True
-                else:
-                    gameover = False
-            else:
-                if (new_board[move.end] == BLUEKING
-                    or (new_board[move.start] == REDKING and move.end == REDGOAL)):
-                    gameover = True
-                else:
-                    gameover = False
-            new_board[move.end] = new_board[move.start]
-            new_board[move.start] = EMPTY
-            new_cards = node.cards[:]
-            # Swap index(move.card) with index 4
-            new_cards[new_cards.index(move.card)] = new_cards[4]
-            new_cards[4] = move.card
-            return Node(
-                board=new_board,
-                prev_move=move,
-                cards=new_cards,
-                children=[],
-                parent=node,
-                end=gameover,
-            )
+    def set_game_as_root(self, game):
+        self.game = game
+        self.card_data = tuple(create_card(oni.CARD_TO_NAME[card]) for card in game.start_cards)
+        cards = [
+            game.start_cards.index(card)
+            for card in game.cards[oni.Player.RED]+game.cards[oni.Player.BLUE]+[game.neutral_card]
+        ]
+        self.root = Node(
+            board=convert_board(self.game.board.array),
+            prev_move=None if len(game.moves) == 0 else convert_move(game.moves[-1]),
+            cards=cards,
+            children=[],
+            parent=None,
+            end=False if game.check_victory() is None else True,
+        )
+
+    def mock_search(self, depth):
         def generate_children(node):
             if not node.end:
-                if node.prev_move is None:
-                    player = self.cards[4].start_player
-                else:
-                    player = (node.prev_move.player+1) % 2
-                pieces = [REDPAWN, REDKING] if player == RED else [BLUEPAWN, BLUEKING]
-                start_squares = [i for i in range(25) if node.board[i] in pieces]
-                player_cards = node.cards[player*2:player*2+2]
-                moves = [
-                    Move(start=start, end=end, player=player, card=card)
-                    for start in start_squares
-                    for card in player_cards
-                    for end in self.cards[card].moves[player][start]
-                    if end not in start_squares
-                ]
                 children = [
-                    apply_move(node, move) for move in moves
-                ]
+                    node.apply_move(move) for move in self.next_moves(node)
+                ]  
                 node.children[:] = children
         frontier = [self.root]
         for _ in range(depth):
             for node in frontier:
                 generate_children(node)
             frontier = [child for node in frontier for child in node.children]
+
+    def mock_search_depth_first(self, depth):
+        def search_children(start_node, depth):
+            if depth <= 0 or start_node.end:
+                return
+            moves = self.next_moves(start_node)
+            start_node.children = [0 for _ in range(len(moves))]
+            for i, move in enumerate(moves):
+                start_node.children[i] = start_node.apply_move(move)
+                search_children(start_node.children[i], depth-1)
+        search_children(self.root, depth)
+
+    def next_moves(self, node):
+        if node.prev_move is None:
+            player = self.card_data[4].start_player
+        else:
+            player = (node.prev_move.player+1) % 2
+        pieces = [REDPAWN, REDKING] if player == RED else [BLUEPAWN, BLUEKING]
+        start_squares = [i for i in range(25) if node.board[i] in pieces]
+        player_cards = node.cards[player*2:player*2+2]
+        return [
+            Move(start=start, end=end, player=player, card=card)
+            for start in start_squares
+            for card in player_cards
+            for end in self.card_data[card].moves[player][start]
+            if end not in start_squares
+        ]
 
     def get_nodes(self, depth):
         frontier = [self.root]
@@ -106,6 +105,35 @@ class Node:
         self.parent = parent
         self.end = end
 
+    def apply_move(self, move):
+        new_board = self.board[:]
+        if move.player == BLUE:
+            if (new_board[move.end] == REDKING
+                or (new_board[move.start] == BLUEKING and move.end == BLUEGOAL)):
+                gameover = True
+            else:
+                gameover = False
+        else:
+            if (new_board[move.end] == BLUEKING
+                or (new_board[move.start] == REDKING and move.end == REDGOAL)):
+                gameover = True
+            else:
+                gameover = False
+        new_board[move.end] = new_board[move.start]
+        new_board[move.start] = EMPTY
+        new_cards = self.cards[:]
+        # Swap index(move.card) with index 4
+        new_cards[new_cards.index(move.card)] = new_cards[4]
+        new_cards[4] = move.card
+        return Node(
+            board=new_board,
+            prev_move=move,
+            cards=new_cards,
+            children=[],
+            parent=self,
+            end=gameover,
+        )
+
 class Move:
     __slots__ = ['start', 'end', 'source', 'target', 'player', 'card', 'neutral_card']
 
@@ -118,11 +146,13 @@ class Move:
 class Card:
     __slots__ = ['moves', 'start_player', 'name']
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def __init__(self, moves, start_player, name):
+        self.moves = moves
+        self.start_player = start_player
+        self.name = name
 
-def card_from_onicard(card):
+def create_card(card_name):
+    card = oni.NAME_TO_CARD[card_name]
     red_disps = [c[0]+c[1]*5 for c in card.moves[oni.Player.RED]]
     blue_disps = [c[0]+c[1]*5 for c in card.moves[oni.Player.BLUE]]
     red_moves = tuple(
@@ -134,18 +164,18 @@ def card_from_onicard(card):
     return Card(
         moves=(red_moves, blue_moves),
         start_player=RED if card.start_player == oni.Player.RED else BLUE,
-        name=oni.CARD_TO_NAME[card],
+        name=card_name,
     )
 
-def move_from_onimove(move):
+def convert_move(move):
     return Move(
         start=move.start[0] + move.start[1]*5,
         end=move.end[0] + move.end[1]*5,
         player=RED if move.player == oni.Player.RED else BLUE,
-        card=card_from_onicard(move.card)
+        card=convert_card(move.card)
     )
 
-def node_from_game(game):
+def convert_board(board):
     pieces = {
         oni.Piece.EMPTY: EMPTY,
         oni.Piece.R_PAWN: REDPAWN,
@@ -153,12 +183,4 @@ def node_from_game(game):
         oni.Piece.B_PAWN: BLUEPAWN,
         oni.Piece.B_KING: BLUEKING,
     }
-    cards = game.cards[oni.Player.RED] + game.cards[oni.Player.BLUE] + [game.neutral_card]
-    return Node(
-        board=[pieces[p] for p in game.board.array],
-        prev_move=None if len(game.moves) == 0 else move_from_onimove(game.moves[-1]),
-        cards=[game.start_cards.index(card) for card in cards],
-        children=[],
-        parent=None,
-        end=True if game.check_victory() is not None else False,
-    )
+    return [pieces[p] for p in board]
