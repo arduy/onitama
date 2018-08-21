e429d875d58c571864a16a398b82c77ee4bc0d9a
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-21 15:53:05 -0700

    Refactor to using __slots__

--- ai2.py
+++ ai2.py
@@ -41,10 +41,10 @@ class AI:
         start_squares = [i for i in range(25) if self.board[i] in pieces]
         player_cards = self.cards[self.active_player*2:self.active_player*2+2]
         return [
-            Move(start, end, self.board[end], self.active_player, card, self.cards[4])
+            Move(start=start, end=end, target=self.board[end], player=self.active_player, card=card, neutral_card=self.cards[4])
             for start in start_squares
             for card in player_cards
-            for end in self.card_data[card][0][self.active_player][start]
+            for end in self.card_data[card].moves[self.active_player][start]
             if end not in start_squares
         ]
 
@@ -53,59 +53,82 @@ class AI:
         return 0
 
     def do_move(self, move):
-        source = self.board[move[0]]
-        if source == REDKING and move[1] == REDGOAL:
+        source = self.board[move.start]
+        if source == REDKING and move.end == REDGOAL:
             gameover = True
-        elif source == BLUEKING and move[1] == BLUEGOAL:
+        elif source == BLUEKING and move.end == BLUEGOAL:
             gameover = True
-        elif move[2] == REDKING or move[2] == BLUEKING:
+        elif move.target == REDKING or move.target == BLUEKING:
             gameover = True
         else:
             gameover = False
-        self.board[move[1]] = self.board[move[0]]
-        self.board[move[0]] = EMPTY
-        self.cards[self.cards.index(move[4])] = self.cards[4]
-        self.cards[4] = move[4]
+        self.board[move.end] = self.board[move.start]
+        self.board[move.start] = EMPTY
+        self.cards[self.cards.index(move.card)] = self.cards[4]
+        self.cards[4] = move.card
         self.active_player = (self.active_player+1)%2
         return gameover
 
     def undo_move(self, move):
-        self.board[move[0]] = self.board[move[1]]
-        self.board[move[1]] = move[2]
-        self.cards[self.cards.index(move[5])] = move[4]
-        self.cards[4] = move[5]
+        self.board[move.start] = self.board[move.end]
+        self.board[move.end] = move.target
+        self.cards[self.cards.index(move.neutral_card)] = move.card
+        self.cards[4] = move.neutral_card
         self.active_player = (self.active_player+1)%2
 
     def mock_search(self, depth):
         def search_children(start_node, depth):
-            if depth <= 0 or start_node[4]:
+            if depth <= 0 or start_node.end:
                 return
             moves = self.next_moves()
-            children = [0 for _ in range(len(moves))]
+            start_node.children = [0 for _ in range(len(moves))]
             for i, move in enumerate(moves):
                 gameover = self.do_move(move)
-                children[i] = Node(
+                start_node.children[i] = Node(
                     prev_move=move,
                     children=[],
                     parent=start_node,
                     end=gameover,
                     eval=0
                 )
-                search_children(children[i], depth-1)
+                search_children(start_node.children[i], depth-1)
                 self.undo_move(move)
-            start_node[2][:] = children
         search_children(self.root, depth)
 
     def get_nodes(self, depth):
         nodes = [self.root]
         for _ in range(depth):
-            nodes = [child for node in nodes for child in node[2]]
+            nodes = [child for node in nodes for child in node.children]
         return nodes
 
+class Node:
+    __slots__ = ['prev_move', 'children', 'parent', 'end', 'eval']
 
-Node = namedtuple('Node', ['eval', 'prev_move', 'children', 'parent', 'end'])
-Move = namedtuple('Move', ['start', 'end', 'target', 'player', 'card', 'neutral_card'])
-Card = namedtuple('Card', ['moves', 'start_player', 'name'])
+    def __init__(self, prev_move, children, parent, end, eval):
+        self.prev_move = prev_move
+        self.children = children
+        self.parent = parent
+        self.end = end
+        self.eval = eval
+
+class Move:
+    __slots__ = ['start', 'end', 'target', 'player', 'card', 'neutral_card']
+
+    def __init__(self, start, end, target, player, card, neutral_card):
+        self.start = start
+        self.end = end
+        self.target = target
+        self.player = player
+        self.card = card
+        self.neutral_card = neutral_card
+
+class Card:
+    __slots__ = ['moves', 'start_player', 'name']
+
+    def __init__(self, moves, start_player, name):
+        self.moves = moves
+        self.start_player = start_player
+        self.name = name
 
 def create_card(card_name):
     card = oni.NAME_TO_CARD[card_name]
