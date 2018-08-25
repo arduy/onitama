75383a6e7dd35bb5b36b3b20bed5a3e8f63298cd
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-24 21:50:00 -0700

    Draw pieces on board

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from PIL import ImageTk, Image
+import onitama as oni
 
 class GUI():
     rows = 5
@@ -7,15 +9,26 @@ class GUI():
     offset = 5
     dark = '#f5deb3'
     light = '#ffffe0'
-    def __init__(self, parent):
+    piece_names = ['redpawn', 'bluepawn', 'redking', 'blueking']
+    def __init__(self, parent, flip=False):
         self.parent = parent
+        self.flip = flip
         self.canvas = Canvas(
             parent,
             width=self.columns*self.square_size+self.offset*2,
             height=self.rows*self.square_size+self.offset*2,
         )
         self.canvas.pack()
-        self.draw_board()
+        self.draw_board(self.flip)
+
+        images = {
+            name: Image.open('./pieces/{}.png'.format(name)).resize((self.square_size, self.square_size), Image.ANTIALIAS)
+            for name in self.piece_names
+        }
+        self.images = {
+            name: ImageTk.PhotoImage(image)
+            for name, image in images.items()
+        }
 
     def draw_board(self, flip=False):
         def red_home(row, col):
@@ -46,10 +59,32 @@ class GUI():
                     fill=color
                 )
 
+    def draw_pieces(self, pieces):
+        def coordinate(i):
+            if self.flip:
+                col = i % 5
+                row = i // 5
+                return (col*self.square_size+self.offset, row*self.square_size+self.offset)
+            else:
+                col = i % 5
+                row = 4 - (i // 5)
+                return (col*self.square_size+self.offset, row*self.square_size+self.offset)
+        self.canvas.delete('piece')
+        for i, piece in enumerate(pieces):
+            if piece.name() != 'empty':
+                self.canvas.create_image(
+                    coordinate(i),
+                    anchor='nw',
+                    image=self.images[piece.name()],
+                    tags=('piece', piece.name())
+                )
+
 def main():
     root = Tk()
     root.title('Board')
-    gui = GUI(root)
+    gui = GUI(root,flip=False)
+    game = oni.Game(oni.ALL_CARDS[0:5])
+    gui.draw_pieces(game.board.array)
     root.mainloop()
 
 if __name__ == '__main__':
