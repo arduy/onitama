5043d21b8d089828c2f44db6375e0ad175b0b460
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-24 17:28:22 -0700

    Revert to using Enum class

--- onitama.py
+++ onitama.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from enum import Enum
 import re
 
 
@@ -29,8 +30,8 @@ class Board:
         #   Source and destination are in bounds
         #   Destination contains enemy piece or is empty
         try:
-            return (Piece.belongs_to(self.get(move.start),move.player)
-                    and not Piece.belongs_to(self.get(move.end), move.player))
+            return (self.get(move.start).belongs_to(move.player)
+                    and not self.get(move.end).belongs_to(move.player))
         except BoardBoundsError:
             return False
 
@@ -76,18 +77,18 @@ class Game:
 
     def do_move(self, move):
         if self.validate_move(move):
-            opp = Player.RED if move.player == Player.BLUE else Player.BLUE
+            opp = move.player.other()
             piece = self.board.get(move.start)
-            if Piece.is_king(piece):
+            if piece.is_king():
                 self.kings[move.player].remove(move.start)
                 self.kings[move.player].add(move.end)
-            elif Piece.is_pawn(piece):
+            elif piece.is_pawn():
                 self.pawns[move.player].remove(move.start)
                 self.pawns[move.player].add(move.end)
             dest_piece = self.board.get(move.end)
-            if Piece.is_king(dest_piece):
+            if dest_piece.is_king():
                 self.kings[opp].remove(move.end)
-            elif Piece.is_pawn(dest_piece):
+            elif dest_piece.is_pawn():
                 self.pawns[opp].remove(move.end)
             self.board.set(move.start, Piece.EMPTY)
             self.board.set(move.end, piece)
@@ -163,7 +164,7 @@ class Move:
     def parse_moves(start_player, move_string):
         try:
             result = []
-            players = [start_player, Player.other(start_player)]
+            players = [start_player, start_player.other()]
             moves = move_string.split(' ')
             x_codes = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'f': 4}
             y_codes = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4}
@@ -184,38 +185,44 @@ class Move:
             raise MoveParseError
 
 
-class Piece:
+class Piece(Enum):
     EMPTY = 0
     R_PAWN = 1
     R_KING = 2
     B_PAWN = 3
     B_KING = 4
 
-    @staticmethod
-    def belongs_to(piece, player):
+    def belongs_to(self, player):
         if player == Player.RED:
-            return (piece == Piece.R_PAWN) or (piece == Piece.R_KING)
+            return self in [Piece.R_KING, Piece.R_PAWN]
         elif player == Player.BLUE:
-            return (piece == Piece.B_PAWN) or (piece == Piece.B_KING)
+            return self in [Piece.B_KING, Piece.B_PAWN]
         else:
             return False
 
-    @staticmethod
-    def is_pawn(piece):
-        return piece in {Piece.R_PAWN, Piece.B_PAWN}
+    def is_pawn(self):
+        return self in [Piece.R_PAWN, Piece.B_PAWN]
 
-    @staticmethod
-    def is_king(piece):
-        return piece in {Piece.R_KING, Piece.B_KING}
+    def is_king(self):
+        return self in [Piece.R_KING, Piece.B_KING]
+
+    def name(self):
+        names = {
+            self.EMPTY: 'empty',
+            self.R_PAWN: 'redpawn',
+            self.B_PAWN: 'bluepawn',
+            self.R_KING: 'redking',
+            self.B_KING: 'blueking',
+        }
+        return names[self]
 
 
-class Player:
+class Player(Enum):
     RED = 0
     BLUE = 1
 
-    @staticmethod
-    def other(player):
-        return Player.RED if player == Player.BLUE else Player.BLUE
+    def other(self):
+        return Player.RED if self == Player.BLUE else Player.BLUE
 
 
 class Card:
@@ -230,6 +237,9 @@ class Card:
         x = start_coord[0]
         y = start_coord[1]
         return {(x+i, y+j) for (i, j) in self.moves[player]}
+
+    def name(self):
+        return CARD_TO_NAME.get(self)
 
 
 MONKEY = Card([(1, -1), (1, 1), (-1, -1), (-1, 1)], Player.BLUE)
