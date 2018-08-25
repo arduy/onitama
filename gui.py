22b490414d92be2c84bc89003be5ff990a7ca4af
Adrian Duynstee <adrian.duy@gmail.com>
2018-08-25 12:10:03 -0700

    Draw cards

--- gui.py
+++ gui.py
@@ -6,21 +6,28 @@ class GUI():
     rows = 5
     columns = 5
     square_size = 100
-    offset = 5
+    inset = 5
     dark = '#f5deb3'
     light = '#ffffe0'
     piece_names = ['redpawn', 'bluepawn', 'redking', 'blueking']
+    card_size = 110
+    board_size = 5*square_size+2*inset
     def __init__(self, parent, flip=False):
         self.parent = parent
         self.flip = flip
-        self.canvas = Canvas(
+        self.board_canvas = Canvas(
             parent,
-            width=self.columns*self.square_size+self.offset*2,
-            height=self.rows*self.square_size+self.offset*2,
+            width=self.board_size,
+            height=self.board_size,
         )
-        self.canvas.pack()
-        self.draw_board(self.flip)
-
+        self.card_canvas = Canvas(
+            parent,
+            width=self.card_size*2+3*self.inset,
+            height=self.board_size,
+        )
+        self.card_canvas.pack(side=LEFT, padx=10, pady=10)
+        self.board_canvas.pack(side=LEFT, padx=10, pady=10)
+        self.draw_board()
         images = {
             name: Image.open('./pieces/{}.png'.format(name)).resize((self.square_size, self.square_size), Image.ANTIALIAS)
             for name in self.piece_names
@@ -30,14 +37,15 @@ class GUI():
             for name, image in images.items()
         }
 
-    def draw_board(self, flip=False):
+
+    def draw_board(self):
         def red_home(row, col):
-            if flip:
+            if self.flip:
                 return row == 0 and col == 2
             else:
                 return row == 4 and col == 2
         def blue_home(row, col):
-            if flip:
+            if self.flip:
                 return row == 4 and col == 2
             else:
                 return row == 0 and col == 2
@@ -50,11 +58,11 @@ class GUI():
                     color = '#ffcccc'
                 else:
                     color = colors[(col+row)%2]
-                self.canvas.create_rectangle(
-                    col*self.square_size+self.offset,
-                    row*self.square_size+self.offset,
-                    (col+1)*self.square_size+self.offset,
-                    (row+1)*self.square_size+self.offset,
+                self.board_canvas.create_rectangle(
+                    col*self.square_size+self.inset,
+                    row*self.square_size+self.inset,
+                    (col+1)*self.square_size+self.inset,
+                    (row+1)*self.square_size+self.inset,
                     width=2,
                     fill=color
                 )
@@ -64,19 +72,64 @@ class GUI():
             if self.flip:
                 col = i % 5
                 row = i // 5
-                return (col*self.square_size+self.offset, row*self.square_size+self.offset)
+                return (col*self.square_size+self.inset, row*self.square_size+self.inset)
             else:
                 col = i % 5
                 row = 4 - (i // 5)
-                return (col*self.square_size+self.offset, row*self.square_size+self.offset)
-        self.canvas.delete('piece')
+                return (col*self.square_size+self.inset, row*self.square_size+self.inset)
+        self.board_canvas.delete('piece')
         for i, piece in enumerate(pieces):
             if piece.name() != 'empty':
-                self.canvas.create_image(
+                self.board_canvas.create_image(
                     coordinate(i),
                     anchor='nw',
                     image=self.images[piece.name()],
                     tags=('piece', piece.name())
+                )
+
+    def draw_cards(self, red, blue, neutral):
+        self.card_canvas.delete('card')
+        coords = [
+            (self.inset, self.inset),
+            (self.inset*2 + self.card_size, self.inset),
+            (self.inset + self.card_size/2, self.board_size/2 - self.card_size/2),
+            (self.inset, self.board_size - self.card_size - self.inset),
+            (self.inset*2 + self.card_size, self.board_size - self.card_size - self.inset)
+        ]
+        r = oni.Player.RED
+        b = oni.Player.BLUE
+        if self.flip:
+            cards = red+[neutral]+blue
+            players = [r,r,b,b,b]
+        else:
+            cards = blue+[neutral]+red
+            players = [b,b,r,r,r]
+        for i in range(5):
+            c = coords[i]
+            self.draw_card(c[0], c[1], cards[i], players[i])
+
+
+    def draw_card(self, x, y, card, player):
+        unit = self.card_size/5
+        if self.flip:
+            squares = [(c[0]+2, c[1]+2) for c in card.moves[player.other()]]
+        else:
+            squares = [(c[0]+2, c[1]+2) for c in card.moves[player]]
+        for col in range(5):
+            for row in range(5):
+                if (col, row) == (2,2):
+                    color = 'darkgrey'
+                elif (col, 4-row) in squares:
+                    color = '#80ff80'
+                else:
+                    color = 'white'
+                self.card_canvas.create_rectangle(
+                    x+col*unit,
+                    y+row*unit,
+                    x+(col+1)*unit,
+                    y+(row+1)*unit,
+                    fill=color,
+                    tags=('card')
                 )
 
 def main():
@@ -85,6 +138,7 @@ def main():
     gui = GUI(root,flip=False)
     game = oni.Game(oni.ALL_CARDS[0:5])
     gui.draw_pieces(game.board.array)
+    gui.draw_cards(game.cards[oni.Player.RED], game.cards[oni.Player.BLUE], game.neutral_card)
     root.mainloop()
 
 if __name__ == '__main__':
